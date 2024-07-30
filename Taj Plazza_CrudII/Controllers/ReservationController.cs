cac3e1627d305e6a39e88c6aee3ac98e9bb441f1
jean morelle KOUDORO <koudorojeanmorelle408@gmail.com>
2024-07-30 19:44:16 +0200

    La methode GetCategorie dans ReservationController marche correctement

---
+++ Taj Plazza_CrudII/Controllers/ReservationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taj_Plazza.Core.Interface;
+using Taj_Plazza.Core.Repertory;
+
+namespace Taj_Plazza_CrudII.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationServices reservationServices;
+
+        public ReservationController(IReservationServices reservationServices)
+        {
+            this.reservationServices = reservationServices;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetCategories(string? filter)
+        {
+            var categories = await reservationServices.GetCategoriesAsync(filter);
+
+            if (categories == null || !categories.Any())
+            {
+                return NotFound("Aucune catégorie trouvée.");
+            }
+
+            return Ok(categories);
+        }
+    }
+}
