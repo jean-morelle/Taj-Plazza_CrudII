b041361a978c6fddaa8283a5d200f351142ef3ca
jean morelle KOUDORO <koudorojeanmorelle408@gmail.com>
2024-08-20 18:00:14 +0200

    Update et Delete marche correctement

--- Taj Plazza_CrudII/Controllers/ClientController.cs
+++ Taj Plazza_CrudII/Controllers/ClientController.cs
@@ -13,7 +13,7 @@ namespace Taj_Plazza_CrudII.Controllers
         private readonly IClientServices clientServices;
         private readonly IMapper mapper;
 
-        public ClientController( IClientServices clientServices ,IMapper mapper)
+        public ClientController(IClientServices clientServices, IMapper mapper)
         {
             this.clientServices = clientServices;
             this.mapper = mapper;
@@ -25,14 +25,14 @@ namespace Taj_Plazza_CrudII.Controllers
             try
             {
                 var client = await clientServices.GetAll();
-              
+
                 return Ok(client);
             }
             catch
             {
                 // Gérez l'exception ici (par exemple, en renvoyant un code d'erreur approprié).
                 return StatusCode(500, "Erreur lors de la récupération des clients.");
-            }  
+            }
         }
 
         [HttpGet("{id}")]
@@ -51,10 +51,47 @@ namespace Taj_Plazza_CrudII.Controllers
 
             await clientServices.Create(client);
 
-           
+
             return CreatedAtAction(nameof(GetClient), new { id = client.Id }, client);
         }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateClient(int id, ClientDto clientDto)
+        {
+            var client = await clientServices.GetById(id);
+
+            if (client == null)
+            {
+                return NotFound("Client not found.");
+            }
+
+            // Map the incoming DTO to the existing client entity
+            mapper.Map(clientDto, client);
+
+            await clientServices.Update(client);
+
+            return Ok("Mise à jour avec succès.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var client = await clientServices.GetById(id);
+
+            if (client == null)
+            {
+                return NotFound("Client not found.");
+            }
+
+            await clientServices.Delete(id);
+
+            return NoContent();
+        }
+
+
     }
 }
+
 
 
