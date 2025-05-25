c0174833311f70445271943453f47cd7f2d962c0
HP <HP@DESKTOP-KBHGV17>
2025-05-25 18:22:47 +0000

    recupereration des donnes d espace get

--- Taj Plazza_CrudII/Controllers/EspaceController.cs
+++ Taj Plazza_CrudII/Controllers/EspaceController.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Taj_Plazza_CrudII.Extention;
 using Taje__Plazza.Domain.Interface;
-using Taje__Plazza.Domain.Models;
-using Taje_Plazza.Application.Automapper.DTOs;
-using Taje_Plazza.Application.Services;
 
 namespace Taj_Plazza_CrudII.Controllers
 {
@@ -13,48 +10,20 @@ namespace Taj_Plazza_CrudII.Controllers
     public class EspaceController : ControllerBase
     {
         private readonly IEspaceServices espaceServices;
-        private readonly IEquipementServices services;
+        private readonly IEquipementServices equipementServices;
 
-        public EspaceController(IEspaceServices espaceServices, IEquipementServices services)
+        public EspaceController(IEspaceServices espaceServices,IEquipementServices equipementServices)
         {
             this.espaceServices = espaceServices;
-            this.services = services;
+            this.equipementServices = equipementServices;
         }
         [HttpGet]
-        public async Task<IActionResult> ObtenirEspacesAsync()
+        public async Task<IActionResult> GetAllEspaces()
         {
             var espaces = await espaceServices.ObtenirTousLesEspaceAsync();
-            var equipements = await services.ObtenirTousLesEquipemntsAsync();
-            var espaceDto = espaces.ConvertToDto(equipements);
-            return Ok(espaceDto);
+            var equipements = await equipementServices.ObtenirTousLesEquipemntsAsync();
+            var result = espaces.ConvertToDto(equipements);
+            return Ok(result);
         }
-        [HttpGet("{Id}")]
-        public async Task<IActionResult> ObtenirEspaceParId(Guid id)
-        {
-            var espace = await espaceServices.ObtenirEspaceAsync(id);
-            return Ok(espace);
-        }
-        [HttpPost]
-        public async Task<IActionResult> AjouterEspace(AddEspaceDto addEspaceDto)
-        {
-            var espace = addEspaceDto.ConvertToEntity();
-            await espaceServices.AjouterEspaceAsync(espace);
-            return   CreatedAtAction(nameof(ObtenirEspaceParId), new { id = espace.Id }, espace);
-        }
-        [HttpDelete]
-        public async Task<IActionResult>SupprimerEspace(Guid id)
-        {
-            var espace = await espaceServices.ObtenirEspaceAsync(id);
-            if (espace == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                await espaceServices.SupprimerEspaceAsync(id);
-            }
-            return NoContent();
-        }
-      
-    }
+       
 }
---
+++ Taj Plazza_CrudII/Extention/EspaceExtention.cs
@@ -0,0 +1,26 @@
+﻿using Taje__Plazza.Domain.Models;
+using Taje_Plazza.Application.Automapper.DTOs;
+
+namespace Taj_Plazza_CrudII.Extention
+{
+    public static class EspaceExtention
+    {
+        public static IEnumerable<EspaceReadDto> ConvertToDto(this IEnumerable<Espace> espaces, IEnumerable<Equipement> equipements)
+        {
+            var result = (
+                from espace in espaces
+                join equipement in equipements on espace.EquipementId equals equipement.Id
+                select new EspaceReadDto
+                {
+                    Id = espace.Id,
+                    Nom = espace.Nom,
+                    Capacite = espace.Capacite,
+                    Type = espace.TypeEspace,
+                    NomEquipement = equipement.Nom,
+                    TypeEquipement = equipement.Type
+                }).ToList();
+            return result;
+ 
+        }
+    }
+}
